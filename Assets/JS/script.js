f4841be0016d945c3c06ec185688197037556869
mgetz34 <mgetz340@gmail.com>
2022-11-04 21:23:40 -0600

    added acceptance criteria to js file

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,3 +1,4 @@
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -21,3 +22,19 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+// Acceptance Criteria
+
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+// WHEN I click into a timeblock
+// THEN I can enter an event
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
