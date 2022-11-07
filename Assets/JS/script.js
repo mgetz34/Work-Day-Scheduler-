803b0b5d7f282f3c2537997bad02d5d5cf59b991
mgetz34 <mgetz340@gmail.com>
2022-11-07 09:42:51 -0700

    added a function to the js file

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -21,84 +21,88 @@
 
 //Add code to display the current date in the header of the page.
 
-function headIncrement() {
-  var headTime = dayjs()
-  // console.log(headTime);
-  var formatHead = headTime.format("dddd, MMM, D, YYYY");
-  var currentTime = $("#timeHead")
-  currentTime.text(formatHead)
-};
+$(document).ready(function () {
 
-setInterval(headIncrement, 1000)
-setInterval(checkTime, 1000)
+  function headIncrement() {
+    var headTime = dayjs()
+    // console.log(headTime);
+    var formatHead = headTime.format("dddd, MMM, D, YYYY");
+    var currentTime = $("#timeHead")
+    currentTime.text(formatHead)
+  };
 
-function checkTime() {
-  var timeChecker = dayjs()
-  var currentMTime = timeChecker.format("H")
-  var containerLg = $(".container-lg").children("div")
+  setInterval(headIncrement, 1000)
+  setInterval(checkTime, 1000)
 
-  containerLg.each(function () {
-    var timeId = $(this).attr("id")
+  function checkTime() {
+    var timeChecker = dayjs()
+    var currentMTime = timeChecker.format("H")
+    var containerLg = $(".container-lg").children("div")
 
-    var timeIdSplit = timeId.replace("hour-", "")
+    containerLg.each(function () {
+      var timeId = $(this).attr("id")
 
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
+      var timeIdSplit = timeId.replace("hour-", "")
 
-    if (parseInt(timeIdSplit) > currentMTime) {
-      $(this).removeClass("present")
-      $(this).removeClass("past")
-      $(this).addClass("future")
-    }
-    if (parseInt(timeIdSplit) < currentMTime) {
-      $(this).removeClass("present")
-      $(this).addClass("past")
-      $(this).removeClass("future")
+      // TODO: Add code to apply the past, present, or future class to each time
+      // block by comparing the id to the current hour. HINTS: How can the id
+      // attribute of each time-block be used to conditionally add or remove the
+      // past, present, and future classes? How can Day.js be used to get the
+      // current hour in 24-hour time?
 
-    } if (parseInt(timeIdSplit) == currentMTime) {
-      $(this).addClass("present")
-      $(this).removeClass("past")
-      $(this).removeClass("future")
-    }
+      if (parseInt(timeIdSplit) > currentMTime) {
+        $(this).removeClass("present")
+        $(this).removeClass("past")
+        $(this).addClass("future")
+      }
+      if (parseInt(timeIdSplit) < currentMTime) {
+        $(this).removeClass("present")
+        $(this).addClass("past")
+        $(this).removeClass("future")
+
+      } if (parseInt(timeIdSplit) == currentMTime) {
+        $(this).addClass("present")
+        $(this).removeClass("past")
+        $(this).removeClass("future")
+      }
+    });
+  }
+
+
+  // TODO: Add a listener for click events on the save button. This code should
+  // use the id in the containing time-block as a key to save the user input in
+  // local storage. HINT: What does `this` reference in the click listener
+  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be
+  // useful when saving the description in local storage?
+
+  $(function () {
+
+    btn.on("click", function (event) {
+      var text = $(this).prev().val()
+      var parentId = $(this).parent().attr("id")
+      console.log(text);
+      console.log(parentId);
+
+      localStorage.setItem(parentId, text)
+
+    });
+
+    // TODO: Add code to get any user input that was saved in localStorage and set
+    // the values of the corresponding textarea elements.
+
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"))
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"))
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"))
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"))
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"))
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"))
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"))
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"))
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"))
+
   });
-}
 
-
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-
-$(function () {
-
-  btn.on("click", function (event) {
-    var text = $(this).prev().val()
-    var parentId = $(this).parent().attr("id")
-    console.log(text);
-    console.log(parentId);
-
-    localStorage.setItem(parentId, text)
-
-  });
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements.
-
-  $("#hour-9 .description").val(localStorage.getItem("hour-9"))
-  $("#hour-10 .description").val(localStorage.getItem("hour-10"))
-  $("#hour-11 .description").val(localStorage.getItem("hour-11"))
-  $("#hour-12 .description").val(localStorage.getItem("hour-12"))
-  $("#hour-13 .description").val(localStorage.getItem("hour-13"))
-  $("#hour-14 .description").val(localStorage.getItem("hour-14"))
-  $("#hour-15 .description").val(localStorage.getItem("hour-15"))
-  $("#hour-16 .description").val(localStorage.getItem("hour-16"))
-  $("#hour-17 .description").val(localStorage.getItem("hour-17"))
+  var btn = $(".btn")
 
 });
-
-var btn = $(".btn")
